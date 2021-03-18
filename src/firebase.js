59ac91b76c337bd606acc0901e74bc8732d14bfa
amitosai <atencion@amitosai.com>
2021-03-18 12:42:51 -0300

    Prevent duplicated packages when scann twice

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMO6LYoJtD1UrXQMI-zYznYFILkUGx4iA",
-  authDomain: "scannsend-707df.firebaseapp.com",
-  databaseURL: "https://scannsend-707df.firebaseio.com",
-  projectId: "scannsend-707df",
-  storageBucket: "scannsend-707df.appspot.com",
-  messagingSenderId: "300233406733",
-  appId: "1:300233406733:web:b3b909fdc60891ccc6d844"
+  apiKey: "AIzaSyAahLtzz0JF7vM6apuFLLlYPXmGKVjDJqM",
+  authDomain: "scannsend-amitosai.firebaseapp.com",
+  projectId: "scannsend-amitosai",
+  storageBucket: "scannsend-amitosai.appspot.com",
+  messagingSenderId: "867434139780",
+  appId: "1:867434139780:web:666a8589a100634d26b2d1"
 };
 
 firebase.initializeApp(firebaseConfig);
