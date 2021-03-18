4651e6250b6881dc08896f040ebda7f104a04148
amitosai <atencion@amitosai.com>
2021-03-18 12:57:08 -0300

    Hide Firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAahLtzz0JF7vM6apuFLLlYPXmGKVjDJqM",
-  authDomain: "scannsend-amitosai.firebaseapp.com",
-  projectId: "scannsend-amitosai",
-  storageBucket: "scannsend-amitosai.appspot.com",
-  messagingSenderId: "867434139780",
-  appId: "1:867434139780:web:666a8589a100634d26b2d1"
+  // Credentials
 };
 
 firebase.initializeApp(firebaseConfig);
